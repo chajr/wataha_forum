bb928168aef706a19a01fd14fd04838d33f759b3
Michał Adamiak <michal.adamiak@lizardmedia.pl>
2019-07-21 10:34:09 +0200

    Added configuration ito repository & replace login/pass credentials by env vars

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'localhost';
+$dbport = '3306';
+$dbname = getenv('WATAHA_DB_LOGIN');
+$dbuser = getenv('WATAHA_DB_LOGIN');
+$dbpasswd = getenv('WATAHA_DB_PASS');
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
